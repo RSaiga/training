4d476386bb5b3c90a56c7b8ad9403f1706f66c04
Naruhiko Toda <n_toda@NaruhikonoMacBook-Pro.local>
2023-04-27 09:11:58 +0900

    format

--- python/trainer/src/vending_machine.py
+++ python/trainer/src/vending_machine.py
@@ -1,21 +1,22 @@
 class VendingMachine:
-  drinks = {'water':{'value': 100, 'stock': 5}, 'cola':{'value': 120, 'stock': 5}}
-  amount = 0
-  def put_money(self, money):
-      if self.is_money_usable(money):
-        self.amount = self.amount + money
-        return True
-      else:
-        return False
+    drinks = {'water': {'value': 100, 'stock': 5}, 'cola': {'value': 120, 'stock': 5}}
+    amount = 0
 
-  def get_amount(self):
-      return self.amount
+    def put_money(self, money):
+        if self.is_money_usable(money):
+            self.amount = self.amount + money
+            return True
+        else:
+            return False
 
-  def is_money_usable(self, money):
-      return money not in [1, 5, 5000, 10000]
+    def get_amount(self):
+        return self.amount
 
-  def buy_drink(self, drink):
-      if self.amount >= self.drinks[drink]['value'] and 0 <= self.drinks[drink]['stock']:
-          self.drinks[drink]['stock'] = self.drinks[drink]['stock'] - 1
-          self.amount = self.amount - self.drinks[drink]['value']
-          return self.amount
+    def is_money_usable(self, money):
+        return money not in [1, 5, 5000, 10000]
+
+    def buy_drink(self, drink):
+        if self.amount >= self.drinks[drink]['value'] and 0 <= self.drinks[drink]['stock']:
+            self.drinks[drink]['stock'] = self.drinks[drink]['stock'] - 1
+            self.amount = self.amount - self.drinks[drink]['value']
+            return self.amount
