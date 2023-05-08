399debd018ea9dfd0831fc941effbbd7cb7f26a7
Ryota Saiga <>
2023-05-08 11:53:28 +0900

    add team folder

---
+++ typescript/teamE/jest.config.js
@@ -0,0 +1,13 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    roots: ["<rootDir>/test", "<rootDir>/src"],
+    collectCoverage: true,
+    collectCoverageFrom: [
+        "**/*.ts",
+        "!**/node_modules/**",
+    ],
+    coverageDirectory: 'coverage_dir',
+    coverageReporters: ["html"]
+};
