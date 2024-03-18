81a9d3b956594b768f815ddf392ec825577df981
Ryota Saiga <>
2024-03-18 09:15:43 +0900

    mod

--- typescript/trainer/jest.config.js
+++ typescript/trainer/jest.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
-    roots: ["<rootDir>/test", "<rootDir>/src"],
+    roots: ["<rootDir>/tests", "<rootDir>/src"],
     collectCoverage: true,
     collectCoverageFrom: [
         "**/*.ts",
