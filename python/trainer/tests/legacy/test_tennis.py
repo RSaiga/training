3280ea837f691b32b05200b1d9a380c972a67b29
ryota saiga <ryota_saiga@trustbank.co.jp>
2024-03-27 11:42:16 +0900

    fix

--- python/trainer/tests/legacy/test_tennis.py
+++
@@ -1,8 +0,0 @@
-from src.legacy.tennis import TennisGame1
-
-
-class TestTennis:
-  def test_sample(self):
-    game = TennisGame1('p1', 'p2')
-    game.won_point('p1')
-    assert game.score() == 'Fifteen-Love'
